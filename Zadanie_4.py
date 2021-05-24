a17aaba4586593aaa985d2a865c126b25dc44327
geekbrains <rustam.kemelbekov@gmail.com>
2021-05-24 21:50:12 +0500

    Добавлены 2-5 задачи

--- Zadanie_4.py
+++ Zadanie_4.py
@@ -6,3 +6,14 @@
 # Необходимо написать программу, открывающую файл на чтение и считывающую построчно данные.
 # При этом английские числительные должны заменяться на русские. Новый блок строк должен записываться
 # в новый текстовый файл.
+
+with open('hw_4.txt', 'r') as f:
+    content = f.read()
+    print(f'{content}')
+
+with open('hw_4_new.txt', 'w') as f:
+    content = content.replace('One', 'Один')
+    content = content.replace('Two', 'Два')
+    content = content.replace('Three', 'Три')
+    content = content.replace('Four', 'Четыре')
+    f.writelines(content)
--- Zadanie_5.py
+++ Zadanie_5.py
@@ -1,3 +1,14 @@
 # 5. Создать (программно) текстовый файл, записать в него программно набор чисел, разделенных пробелами.
 # Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
 
+with open('hw_5.txt', 'w') as f:
+    print(input('Вводите числа, через пробел: '), file=f)
+
+with open('hw_5.txt', 'r') as f:
+    content = f.read()
+    summa = 0
+    for i in content.split(' '):
+        if i.isdigit():
+            summa = summa + int(i)
+
+    print(f"Сумма числе в файле: {summa}")
