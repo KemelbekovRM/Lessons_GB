ed03da019f645379d81e5c63378352ed497def8f
geekbrains <rustam.kemelbekov@gmail.com>
2021-05-27 20:45:26 +0500

    Добавлена задача 6

--- Zadanie_6.py
+++ Zadanie_6.py
@@ -9,3 +9,11 @@
 #
 # Пример словаря:
 # {“Информатика”: 170, “Физика”: 40, “Физкультура”: 30}
+
+mydict = {}
+with open("hw_6.txt", encoding="utf-8") as f:
+    for line in f:
+        name, stats = line.split(':')
+        name_sum = sum(map(int, "".join([i for i in stats if i == ' ' or (i >= '0' and i <= '9')]).split()))
+        mydict[name] = name_sum
+print(f"{mydict}")
