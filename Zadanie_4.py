09e32e093263d04975189a998d67a35483a16810
geekbrains <rustam.kemelbekov@gmail.com>
2021-05-31 21:01:40 +0500

    Задания 4-5

--- Zadanie_4.py
+++ Zadanie_4.py
@@ -6,3 +6,39 @@
 # и 40 (WorkCar) должно выводиться сообщение о превышении скорости.
 # Создайте экземпляры классов, передайте значения атрибутов. Выполните доступ к атрибутам, выведите результат.
 # Выполните вызов методов и также покажите результат.
+
+class Car:
+
+    def __init__(self, name, color, speed, is_police=False):
+        self.name = name
+        self.color = color
+        self.speed = speed
+        self.is_police = is_police
+        print(f"Новая машина: {self.name} (Цвет {self.color}) машина полицейская - {self.is_police}")
+
+    def go(self):
+        print(f"{self.name}: Машина поехала")
+
+    def stop(self):
+        print(f"{self.name}: Машина остановилась")
+
+    def turn(self, direction):
+        print(f"{self.name}: Машина повернула {'налево' if direction == 0 else 'направо'}")
+
+    def show_speed(self):
+        print(f"{self.name}: Скорость автомобиля - {self.speed}")
+
+class TownCar(Car):
+
+    def show_speed(self):
+        return f"{self.name}: Скорость автомобиля - {self.speed} - ПРЕВЫШЕНИЕ СКОРОСТИ!!!" \
+            if self.speed > 40 else f"{self.name}: Скорость автомобиля - {self.speed}"
+
+
+class PoliceCar(Car):
+    def __init__(self, name, color, speed, is_police=True):
+        super().__init__(name, color, speed, is_police)
+
+
+town_car = TownCar("KIA", "blue", 40)
+town_car.go()
--- Zadanie_5.py
+++ Zadanie_5.py
@@ -3,3 +3,35 @@
 # Pencil (карандаш), Handle (маркер). В каждом из классов реализовать переопределение метода draw.
 # Для каждого из классов методы должен выводить уникальное сообщение. Создать экземпляры классов и проверить,
 # что выведет описанный метод для каждого экземпляра.
+
+class Stationary:
+
+    def __init__(self, title="Что то для рисования"):
+        self.title = title
+
+    def draw(self):
+        print(f'Запуск отрисовки {self.title}')
+
+
+class Pen(Stationary):
+    def draw(self):
+        print(f'Запуск отрисовки ручкой! {self.title}')
+
+
+class Pencil(Stationary):
+    def draw(self):
+        print(f'Запуск отрисовки карандашом! {self.title}')
+
+
+class Marker(Stationary):
+    def draw(self):
+        print(f'Запуск отрисовки маркером! {self.title}')
+
+stat = Stationary()
+stat.draw()
+
+mark = Marker()
+pen = Pencil()
+
+mark.draw()
+pen.draw()
